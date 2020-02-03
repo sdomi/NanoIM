71acf61098d827b6cb96cd8f9ff695287ff7ff4a
reds <reds@sakamoto.pl>
2020-02-03 23:49:30 +0100

    multiple services possible

--- __main__.py
+++ __main__.py
@@ -12,33 +12,36 @@ PMWindow, PMWindowQtBaseClass = uic.loadUiType("priv.ui")
 contacts = []
 
 
-class nanoListWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class msgListWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 	def __init__(self):
 		QtWidgets.QMainWindow.__init__(self)
 		Ui_MainWindow.__init__(self)
 		self.setupUi(self)
 		self.listWidget.clicked.connect(lambda: self.openPMWindow(self.listWidget.selectedItems()))
-	def addContact(self, text, id):
+	def addContact(self, text, id, type):
 		_translate = QtCore.QCoreApplication.translate
 		item = QtWidgets.QListWidgetItem()
 		self.listWidget.addItem(item)
 		item = self.listWidget.item(self.listWidget.count()-1)
 		item.setText(_translate(None, text))
-		item.setData(0x0100,id)
+		item.setData(0x0100, id)
+		item.setData(0x0101, type)
 	def populateContacts(self):
-		c = nanoNet.getContacts()
+		c = msgNet.getContacts('matrix')
 		for i in c:
-		    print(i)
-		    self.addContact(i['name'],i['id'])
+		    self.addContact(i['name'], i['id'], i['type'])
 	def openPMWindow(self, selected):
-		self.pm=nanoPMWindow(selected[0].data(0x0100),selected[0].text())
+		id = selected[0].data(0x0100)
+		type = selected[0].data(0x0101)
+		name = selected[0].text()
+		self.pm=msgPMWindow(id, name, type)
 
-class nanoNet():
-	def getContacts():
-		r = requests.post(config['remote']+"getContacts", data= {'token': config['token']})
+class msgNet():
+	def getContacts(type):
+		r = requests.post(config["remote"]+"getContacts", data= {'token': config["token"], 'type': type})
 		return r.json()
 	def getHistory(self, number):
-		r = requests.post(config['remote']+"getHistory", data = {'number':number, 'token':config['token']})
+		r = requests.post(config["remote"]+"getHistory", data = {'number':number, 'token': config["token"], 'type': self.type})
 		print(r.encoding)
 		raw='<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>'
 		for i in r.json():
@@ -46,21 +49,22 @@ class nanoNet():
 		_translate = QtCore.QCoreApplication.translate
 		self.textBrowser.setText(_translate(None, raw))
 	def sendMsg(self):
-		msg = nanoPMWindow.getMessageText(self)
-		nanoPMWindow.clearMessageText(self)
-		r = requests.post(config['remote']+"sendMessage", data = {'recipient':self.number,'msg':msg,'token':config['token']})
-		nanoNet.getHistory(self,self.number)
+		msg = msgPMWindow.getMessageText(self)
+		msgPMWindow.clearMessageText(self)
+		r = requests.post(config["remote"]+"sendMessage", data = {'recipient': self.number, 'msg': msg, 'token': config["token"], 'type': self.type})
+		msgNet.getHistory(self, self.number)
 
-class nanoPMWindow(QtWidgets.QMainWindow, PMWindow):
-	def __init__(self, number, name):
+class msgPMWindow(QtWidgets.QMainWindow, PMWindow):
+	def __init__(self, number, name, type):
 		QtWidgets.QDialog.__init__(self)
 		self.setupUi(self)
 		self.setWindowTitle(name+" - NanoIM")
 		self.number = number
 		self.name = name
-		self.pushButton.clicked.connect(lambda: nanoNet.sendMsg(self))
+		self.type = type
+		self.pushButton.clicked.connect(lambda: msgNet.sendMsg(self))
 		self.show()
-		nanoNet.getHistory(self, number)
+		msgNet.getHistory(self, number)
 	def getMessageText(self):
 		return self.plainTextEdit.toPlainText()
 	def clearMessageText(self):
@@ -68,7 +72,7 @@ class nanoPMWindow(QtWidgets.QMainWindow, PMWindow):
 
 if __name__ == "__main__":
 	app = QtWidgets.QApplication(sys.argv)
-	window = nanoListWindow()
+	window = msgListWindow()
 	window.show()
 	window.populateContacts()
 	sys.exit(app.exec_())
